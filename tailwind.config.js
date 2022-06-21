9e94af23f7a62094876f8aeae7ef68987be559af
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-06-21 22:59:26 +0100

    Install tailwindcss, postcss and autoprefixer

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // which files will contain some tailwindcss styles
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
