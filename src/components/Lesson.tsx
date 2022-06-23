d3c68e3c84d11ff2b6b1bda8d7a3754d4114db48
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-06-23 01:09:00 +0100

    Pull lessons from GraphCMS and install date-fns

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,5 @@
 import { CheckCircle, Lock } from 'phosphor-react';
+import { isPast, format } from 'date-fns';
 
 interface LessonProps {
   title: string;
@@ -8,12 +9,13 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
-  const isLessonAvailable = false;
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormatted = format(props.availableAt, "EEEE' • 'd MMMM' • 'k'h'mm");
 
   return (
     <a href="#">
       <span className="text-gray-300">
-        {props.availableAt.toString()}
+        {availableDateFormatted}
       </span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
@@ -21,7 +23,7 @@ export function Lesson(props: LessonProps) {
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
               <CheckCircle size={20} />
-              Unlocked content
+              Unlocked class
             </span>
           ) : (
             <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
@@ -31,7 +33,7 @@ export function Lesson(props: LessonProps) {
           )}
 
           <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold uppercase">
-            {props.title === 'live' ? 'Live' : 'Practical Class'}
+            {props.type === 'live' ? 'Live' : 'Practical Class'}
           </span>
         </header>
         <strong className="text-gray-200 mt-5 block">
