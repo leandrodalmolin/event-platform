a3fc0a8a79a247fc1971505b3f9cb06d956fa5fe
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-06-26 17:53:25 +0100

    Creates "Subscriber" on GraphCMS (mutation)

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,5 +2,8 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
   uri: import.meta.env.VITE_CMS_CONTENT_API_URI,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_CMS_CONTENT_API_TOKEN}`,
+  },
   cache: new InMemoryCache()
 })
